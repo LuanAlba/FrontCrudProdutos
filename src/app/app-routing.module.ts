c1eae2bf23d80d3d8536524f7ab3a2a24ab3aa50
LuanAlba <luan.alba2@gmail.com>
2024-05-16 17:28:48 -0300

    Começando as views e ajustando as primeiras rotas p navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/views/home/home.component';
+import { ProdutosComponent } from './components/views/produtos/produtos.component';
+
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  {
+    path: "produtos", component: ProdutosComponent
+  } 
+
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
